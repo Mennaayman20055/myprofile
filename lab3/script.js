1de0490dd2be60b582e16ef2858ecac57508c7db
Mennaayman20055 <mennaaymangad2005@gmail.com>
2025-08-05 06:54:19 +0300

    Add files via upload

---
+++ lab3/script.js
@@ -0,0 +1,37 @@
+let name = "menna";
+console.log(" name is"+" "+name);
+
+let grade;
+
+
+let score = 30;
+console.log("score ="+" "+ score);
+
+if(score>=90){
+    
+console.log( grade="A" );
+}
+else if ((score<90) &&(score>80)) {
+    
+    console.log (grade="B");
+ 
+}
+else if ((score<80) &&(score>70)) {
+
+    console.log(grade="C" );
+    
+}else if ((score<70) &&(score>60)) {
+    
+    console.log( grade="D" );
+    
+}else {
+    console.log(grade="your are failed");
+}
+console.log("grade ="+" "+ grade);
+
+if(score>50){
+    console.log("you passed ");
+}
+else{
+    console.log("you failed");
+}
---
+++ lab4/script.js
@@ -0,0 +1,4 @@
+const warning= document.getElementById("warning");
+warning.style.color="red";
+warning.style.fontStyle="bold";
+warning.innerhtml=<span>important message</span>
